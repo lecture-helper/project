d836a6bf4c9c78316e0e69b3e370b8a42e25f7fa
Hanna Koh <hxkoh2@illinois.edu>
2014-11-12 18:00:11 -0600

    changes to database

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,17 +1,49 @@
-CREATE TABLE Class (
-class_name VARCHAR(100) PRIMARY KEY,
-class_key INT 
+CREATE TABLE Person (
+type VARCHAR(10),
+username VARCHAR(100) PRIMARY KEY,
+password VARCHAR(100)
 );
 
 CREATE TABLE Question (
-question_id INT PRIMARY KEY,
+question_tag VARCHAR(1000),
+question_id INTEGER PRIMARY KEY AUTOINCREMENT,
 question_text VARCHAR(1000),
 question_date DATE,
-question_time TIME 
+question_time TIME,
+question_confusion INT
+);
+
+CREATE TABLE Class (
+class_name VARCHAR(100) PRIMARY KEY,
+class_key INT
+);
+
+CREATE TABLE Asks (
+question_id INTEGER,
+username VARCHAR(100),
+FOREIGN KEY(question_id)
+REFERENCES Question(question_id)
+ON DELETE CASCADE
+);
+
+CREATE TABLE Subscribes (
+username VARCHAR(100),
+class_name VARCHAR(100),
+FOREIGN KEY(class_name)
+REFERENCES Class(class_name)
+ON DELETE CASCADE
+ON UPDATE CASCADE
 );
 
 CREATE TABLE Asked_in (
-question_id INT,
-class_name VARCHAR(100) ,
-PRIMARY KEY(question_id, class_name)
+question_id INTEGER,
+class_name VARCHAR(100),
+FOREIGN KEY(class_name)
+REFERENCES Class(class_name)
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+FOREIGN KEY(question_id)
+REFERENCES Question(question_id)
+ON DELETE CASCADE
 );
+
