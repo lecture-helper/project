21007f3a0e625fa2e3c7ac72892cfcddd530f965
Hanna Koh <hxkoh2@illinois.edu>
2014-11-16 08:23:21 -0600

    changes to professor view and added extra functionality

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -59,33 +59,50 @@ def formatTime(t):
 
 
 ##############################Professor Code########################################
+@app.route('/temp')
+def temp():
+	return render_template('temp.html')
+
+@app.route('/add_temp', methods=['POST'])
+def add_temp():
+	username = request.form['username']
+	password = request.form['password']
+	student_prof = request.form['student_prof']
+	cur = g.db.execute('insert into Person(type, username, password) values (?,?,?)', [student_prof, username, password])
+	g.db.commit()
+	flash('Person added')
+	return redirect(url_for('temp'))
 
 @app.route('/professor')
 def professor():
-	cur = g.db.execute('select * from Class')
+	username = 'prof1' #change later and pass through from login
+	cur = g.db.execute('select Class.class_name, Class.class_key from Class, Subscribes where Class.class_name = Subscribes.class_name AND Subscribes.username="'+username+'"')
 	prof_class = [dict(class_name=row[0], class_key=row[1]) for row in cur.fetchall()]
-	return render_template('professor.html', classes=prof_class)
+	prof_username = username
+	return render_template('professor.html', classes=prof_class, prof_username = prof_username)
 
 
-@app.route('/professor_class/<class_name1>')
-def professor_class(class_name1):
+@app.route('/professor_class/<username>/<class_name1>')
+def professor_class(username, class_name1):
 	cur = g.db.execute('select question_text, question_date, question_time,question_confusion from Question where question_id IN (select question_id from Asked_in where class_name="'+class_name1+'")  order by question_date desc, question_time desc')
 	questions = [dict(text=row[0], date=row[1], time=row[2], confusion=row[3]) for row in cur.fetchall()]
-	return render_template('class.html', questions=questions, class_name=class_name1)
+	prof_username = username
+	return render_template('class.html', questions=questions, class_name=class_name1, prof_username = username)
 
 
 @app.route('/add_class', methods=['POST'])
 def add_class():
+	username = request.form['username']
 	class_name = request.form['class_name']
 	class_key = request.form['class_key']
 	if len(class_name) == 0:
-		flash('No class name received and not inserted into db')
+		flash('No class name received and not added')
 		return redirect(url_for('professor'))
 	if len(class_key) == 0:
-		flash('No class key received and not inserted into db')
+		flash('No class key received and not added')
 		return redirect(url_for('professor'))
-	g.db.execute('insert into Class (class_name, class_key) values (?, ?)',
-		[class_name, class_key])
+	g.db.execute('insert into Class (class_name, class_key) values (?, ?)', [class_name, class_key])
+	g.db.execute('insert into Subscribes (username, class_name) values (?, ?)', [username, class_name])
 	g.db.commit()
 	flash('New class added')
 	return redirect(url_for('professor'))
@@ -93,22 +110,62 @@ def add_class():
 
 @app.route('/delete_class', methods=['POST'])
 def delete_class():
+	username = request.form['username']	
+	print username
 	class_name = request.form['class_name']
+	print class_name
 	if len(class_name) == 0:
-		flash('No class name received and not deleted from db')
+		flash('No class name received and not deleted')
 		return redirect(url_for('professor'))
-	g.db.execute('Delete from Class where class_name = (?)',
-		[class_name])
+	g.db.execute('Delete from Class where class_name = (?)', [class_name])
+	g.db.execute('Delete from Subscribes where class_name = (?)', [class_name])
 	g.db.commit()
 	flash('Class deleted')
 	return redirect(url_for('professor'))
 
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+	class_name = request.form['class_name']
+	class_key = request.form['class_key']
+	username = request.form['username']
+	if len(class_name) == 0:
+		flash('No class name received and not subscribed')
+		return redirect(url_for('professor'))
+	if len(class_key) == 0:
+		flash('No class key received and not subscribed')
+		return redirect(url_for('professor'))
+	cur = g.db.execute('select * from Class where class_name ="' + class_name + '"')
+	classes = cur.fetchall()
+	if len(classes) == 0:
+		print 'reached'
+		flash('This class does not exist')
+		return redirect(url_for('professor'))
+	if class_key != classes[0][1]:
+		flash('The key entered is not correct')
+		return redirect(url_for('professor'))
+	g.db.execute('insert into Subscribes (username, class_name) values (?, ?)',[username, class_name])
+	g.db.commit()
+	flash('Subcribed to class')
+	return redirect(url_for('professor'))
 
+@app.route('/unsubscribe', methods=['POST'])
+def unsubscribe():
+	class_name = request.form['class_name']
+	username = request.form['username']
+	cur = g.db.execute('select * from Class where class_name ="' + class_name + '"')
+	if len(cur.fetchall()) == 0:
+		flash('This class does not exist')
+		return redirect(url_for('professor'))
+	g.db.execute('Delete from Subscribes where Subscribes.username="' + username + '" AND Subscribes.class_name="' + class_name + '"')
+	g.db.commit()
+	flash('Unsubscribed from class')
+	return redirect(url_for('professor'))
 
 ##############################Student Code#####################################	
 	
 @app.route('/student')
 def student():
+	username = 'hxkoh2'
 	cur = g.db.execute('select question_text, question_date, question_time, question_confusion from Question where Question.question_id IN (select question_id from Asked_in) order by question_date desc, question_time desc')
 	questions = [dict(text=row[0], date=formatDate(row[1]), time=formatTime(row[2]), confusion=row[3]) for row in cur.fetchall()]
 	return render_template('student.html', questions=questions)
